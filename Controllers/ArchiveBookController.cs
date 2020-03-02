b23721b1ca051b67eced11b89a2be7b8c067b97b
Dobrinka Janakieva <dobrinkajanakieva@gmail.com>
2020-03-02 12:28:07 +0100

    Added Controllers Project

---
+++ Controllers/ArchiveBookController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Controllers
+{
+	class ArchiveBookController : Controller
+	{
+		public ArchiveBookController() : base() { }
+	}
+}
---
+++ Controllers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Controllers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
